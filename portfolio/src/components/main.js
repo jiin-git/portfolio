5659dfda27a0c7c53a42baad8bb168ec8168aeb4
Jiin <jiin.bae99@gmail.com>
2022-08-19 01:09:06 +0900

    main modules

---
+++ portfolio/src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header/header';
+import Body from './body/body';
+import Footer from './footer/footer';
+
+function Main() {
+    return (
+        <div id='main'>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Main;
