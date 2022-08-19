c44d76ae72ae22f315f056f4506191c6b997cab0
Jiin <jiin.bae99@gmail.com>
2022-08-20 00:47:53 +0900

    body top module - introuduce module file 분리 및 img drag none 설정

---
+++ portfolio/src/components/body/top/introduce.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './top.module.css';
+
+function Introduce() {
+    return (
+        <div id={styles.introduce}>
+            배지인의 포트폴리오
+        </div>
+    );
+}
+
+export default Introduce;
--- portfolio/src/components/body/top/top.js
+++ portfolio/src/components/body/top/top.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import TopImg from './topImg';
+import Introduce from './introduce';
 import styles from './top.module.css';
 
 function Top() {
     return (
-        <div className='top_container'>
+        <div className={styles.top_container}>
             <TopImg/>
             <Introduce/>
-        </div>
-    );
-}
-
-function Introduce() {
-    return (
-        <div id={styles.introduce}>
-            배지인의 포트폴리오
         </div>
     );
 }
