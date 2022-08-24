481713a695abe033431bd41bfae4574993ead543
Jiin <jiin.bae99@gmail.com>
2022-08-24 11:23:07 +0900

    react router dom 패키지를 사용하여 app, index js 모듈을 변경했습니다.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import './index.css';
 import Main from './components/main';
+import ThanksFor from './components/footer/thanksfor';
 
 function App() {
   return (
-    <Main/>
+    <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/thanksfor" element={<ThanksFor/>}/>
+    </Routes>
   );
 }
 
