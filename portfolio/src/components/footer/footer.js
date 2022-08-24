bdceaf5b1e44414a9149d0769d8625533724c27e
Jiin <jiin.bae99@gmail.com>
2022-08-24 11:24:52 +0900

    footer module - 링크를 통해 thanksfor 경로로 이동하며 불필요한 html 파일을 제거하고 thanksfor.js를 추가합니다.

--- portfolio/src/components/footer/footer.js
+++ portfolio/src/components/footer/footer.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './footer.module.css';
 
 function Footer() {
+    // useEffect(() => {
+    //     document.title = "Thanks For";
+    // },[]);
+
     return (
         <div className={styles.footer_container}>
             <div className={styles.footer_content}>
                 <h3>Contact: jiin.bae99@gmail.com</h3>
                 <h3>Copyright ©2022 BaeJiIn All rights reserved.</h3>
-                <a>Thanks for</a>
+                <Link to="/thanksfor">Thanks for</Link>
             </div>
         </div>
     );
---
+++ portfolio/src/components/footer/thanksfor.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function ThanksFor() {
+    return (
+        <div>
+            <h1>ThanksFor</h1>
+            <h2>Img</h2>
+            <p>
+                <h3>Freepik</h3>
+                <ul>
+                    <li><a href="https://www.flaticon.com/free-icons/html-5" title="html 5 icons">Html 5 icons created by Freepik - Flaticon</a></li>
+                    <li><a href="https://www.flaticon.com/free-icons/css-3" title="css 3 icons">Css 3 icons created by Freepik - Flaticon</a></li>
+                    <li><a href="https://www.flaticon.com/free-icons/nodejs" title="nodejs icons">Nodejs icons created by Freepik - Flaticon</a></li>
+                    <li><a href="https://www.flaticon.com/free-icons/mysql" title="mysql icons">Mysql icons created by Freepik - Flaticon</a></li>
+                </ul>
+            </p>
+            <p>
+                <h3>Jason Long</h3>
+                <ul>
+                    <li><a href='https://git-scm.com/downloads/logos' title='git icons'>Git icons created by Jason Long</a></li>
+                </ul>
+            </p>
+            <p>
+                <h3>Pixel perfect</h3>
+                <ul>
+                    <li><a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a></li>
+                </ul>
+            </p>
+            <p>
+                <h3>Liblogo</h3>
+                <ul>
+                    <li><a href="https://www.liblogo.com/lib/sourcetree-logo.html" title="Sourcetree Logo">Sourcetree Logo - Liblogo</a></li>
+                </ul>
+            </p>
+        </div>
+        
+    );
+}
+
+export default ThanksFor;
